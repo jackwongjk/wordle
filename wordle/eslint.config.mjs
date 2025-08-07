61b31f399359573bc710b05bafde65ff9f254b21
Jack Wong <jack.wong.jk@outlook.com>
2025-08-08 00:12:06 +0800

    Amend eslint config

--- wordle/eslint.config.mjs
+++ wordle/eslint.config.mjs
@@ -11,7 +11,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals'),
+  ...compat.config({
+    extends: ['next/core-web-vitals'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      'no-undef': 'error',
+    },
+  }),
   eslintPluginPrettierRecommended,
 ];
 
