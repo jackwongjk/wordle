8481b1c514b8a3eb255d89d0fd002d639dc88008
Jack Wong <jack.wong.jk@outlook.com>
2025-08-08 18:14:35 +0800

    Change index page structure

---
+++ wordle/pages/home/Home.js
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Keyboard from '@/components/keyboard';
+import WordleBody from './wordle-body';
+
+const WORDS = process.env.WORDS;
+const MAX_TRIES = process.env.MAX_TRIES;
+
+export const metadata = {
+  title: 'Wordle',
+  description: `Guess the word in ${MAX_TRIES} tries!`,
+};
+
+export default function Home() {
+  return (
+    <div className="font-sans grid items-center justify-items-center min-h-screen p-4 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col items-center sm:items-start w-full">
+        <WordleBody words={WORDS} />
+      </main>
+    </div>
+  );
+}
